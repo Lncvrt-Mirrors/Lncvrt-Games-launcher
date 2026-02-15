15dd9ad0137c1a32defff98cad39cc3f82a43692
Lncvrt <lncvrtreal@gmail.com>
2026-02-14 18:14:23 -0700

    Make logo look better on windows

--- src/componets/Sidebar.tsx
+++ src/componets/Sidebar.tsx
@@ -59,15 +59,10 @@ export default function Sidebar () {
         }}
       ></div>
       <div
-        className='logo'
-        style={{
-          marginTop:
-            platform() == 'windows'
-              ? '32px'
-              : platform() == 'macos'
-              ? '28px'
-              : ''
-        }}
+        className={`flex items-center h-10 w-60 ${
+          (platform() == 'windows' ? 'pl-1 pt-1' : 'pl-2 pt-2') +
+          (platform() == 'macos' ? ' mt-7' : '')
+        }`}
         onMouseDown={e => {
           if (platform() != 'macos') return
           if (e.buttons === 1) {
